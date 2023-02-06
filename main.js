9453f76c1efe918011b8e240f4e348381e0b97f0
Racheal Nyaga <benracheal552@gmail>
2023-02-06 09:29:16 +0300

    add files

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ let descInput = document.querySelector(".description");
 let dateInput = document.querySelector(".date");
 let submitTodo = document.querySelector(".submit-todo");
 
-let savedTodosList = document.querySelector(".list-todos")
+let uncompletedTodosList = document.querySelector(".uncompleted-list-todos")
+let completedTodosList = document.querySelector(".completed-list-todos")
 
-let title = "";
-let desc = "";
-let date = "";
+
 
 let todos = JSON.parse(localStorage.getItem("todos"));
 
@@ -20,45 +19,61 @@ descInput.addEventListener("input", function(event) {
 })
 
 dateInput.addEventListener("input", function(event) {
-    date = event.target.value;
+    let today = new Date();
+    let duedate = new Date (event.target.value);
+    if(duedate<= today){
+        alert("Date cannot be in the past")
+    } else {
+        date = event.target.value;
+    }
 })
 
 submitTodo.addEventListener("click", function(event) {
     event.preventDefault();
 
-    let newTodo = { // make new todo an object with key: value pairs
-        "id": Math.round(Math.random() * 1000),
+    let newTodo = { 
+        "id": Math.round(Math.random() * 2000),
         "title": title,
         "description": desc,
         "date": date,
         "completed": false
     }
 
-    // Save our values
+    
     if(todos) {
         todos.push(newTodo);
-        localStorage.setItem("todos", JSON.stringify(todos)); // update todos in localStorage
+        localStorage.setItem("todos", JSON.stringify(todos)); 
 
     } else {
-        let newTodos = [ // create new array with our new todo
+        let newTodos = [ 
             newTodo
         ];
 
-        localStorage.setItem("todos", JSON.stringify(newTodos)); // save new todo in localStorage
+        localStorage.setItem("todos", JSON.stringify(newTodos));
     }
     location.reload();
 })
 
-/** Populate the todos list  */
+
 for(let i = 0; i < todos.length; i++) {
-    savedTodosList.innerHTML += `<li ${todos[i].completed ? "class='completed'" : ''}>
+    if(todos[i].completed) { // check if todo is completed
+        completedTodosList.innerHTML += `<li class="completed">
         <p>${todos[i].title}</p>
         <p>${todos[i].description}</p>
         <small>Due date: ${todos[i].date}</small>
         <button class="complete-todo" data-id="${todos[i].id}"> ${todos[i].completed ? "<i class='fa-solid fa-rectangle-xmark'></i>" : "<i class='fa-solid fa-check'></i>"}</button>
         <button class="delete" data-id="${todos[i].id}"><i class='fa-solid fa-trash'></i></button>
     </li>`;
-}
+    } 
+    else { //if todo is not completed
+        uncompletedTodosList.innerHTML += `<li>
+        <p>${todos[i].title}</p>
+        <p>${todos[i].description}</p>
+        <small>Due date: ${todos[i].date}</small>
+        <button class="complete-todo" data-id="${todos[i].id}"> ${todos[i].completed ? "<i class='fa-solid fa-rectangle-xmark'></i>" : "<i class='fa-solid fa-check'></i>"}</button>
+        <button class="delete" data-id="${todos[i].id}"><i class='fa-solid fa-trash'></i></button>
+    </li>`;
+    }
 
 
 // ${variablename}
@@ -87,7 +102,7 @@ completeButtons.forEach(function(button) {
 
 
 
-// Delete a todo
+
 let deleteButtons = document.querySelectorAll(".delete"); // get all delete buttons
 
 deleteButtons.forEach(function(button) { // loop thru buttons list to get each button
@@ -105,4 +120,4 @@ deleteButtons.forEach(function(button) { // loop thru buttons list to get each b
         localStorage.setItem("todos", JSON.stringify(todos)); // re-save new todo in localStorage
         location.reload();
     })
-})
+})}
